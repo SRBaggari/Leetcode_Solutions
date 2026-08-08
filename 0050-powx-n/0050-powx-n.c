03f36c7f441d044ed725bc9960fef5edc755acc5
SAHASRA REDDY BAGGARI <baggarisahasra07@gmail.com>
2026-08-08 00:26:13 -0400

    Time: 0 ms (100%), Space: 8.8 MB (18.53%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.c
@@ -0,0 +1,19 @@
+double myPow(double x, int n) {
+    long long power = n;   // use long long to handle INT_MIN
+    double result = 1.0;
+
+    if (power < 0) {
+        x = 1 / x;
+        power = -power;
+    }
+
+    while (power > 0) {
+        if (power % 2 == 1) {
+            result *= x;
+        }
+        x *= x;
+        power /= 2;
+    }
+
+    return result;
+}
